8e6255db05d143b63197764ed66e649761b4fc30
Johann Dirry <movgp0@gmail.com>
2016-10-08 13:28:51 +0200

    finishing chrome styling

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
-using System.Windows.Documents;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using Calculator.Controls;
 
 namespace Calculator
@@ -10,6 +12,14 @@ namespace Calculator
         {
             InitializeComponent();
             Loaded += OnLoaded;
+
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
+
+            var iconUri = new Uri("pack://application:,,,/Assets/Square150x150Logo.scale-200.ico", UriKind.RelativeOrAbsolute);
+            Icon = BitmapFrame.Create(iconUri);
         }
 
         protected void OnLoaded(object sender, RoutedEventArgs e)
@@ -19,6 +29,36 @@ namespace Calculator
 
             //AdornerLayer.GetAdornerLayer(RootElement).Add(rootAdorner);
             //AdornerLayer.GetAdornerLayer(Fraction).Add(fractionAdorner);
+        }
+
+        private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ResizeMode != ResizeMode.NoResize;
+        }
+
+        private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ResizeMode == ResizeMode.CanResize || ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private void OnCloseWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void OnMaximizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void OnMinimizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void OnRestoreWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
         }
     }
 }
